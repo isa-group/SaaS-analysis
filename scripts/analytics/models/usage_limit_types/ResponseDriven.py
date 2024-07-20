ad8b017a8ab049cf66f79d9b8c042b27a5384649
Francisco Javier Cavero López <javiercavlop@gmail.com>
2024-07-20 19:44:04 +0200

    REFACTOR: RELEASE VERSION

--- scripts/analytics/models/usage_limit_types/ResponseDriven.py
+++
@@ -1,9 +0,0 @@
-from models import UsageLimit, UsageLimitType
-
-class ResponseDriven(UsageLimit):
-    def __init__(self):
-        super().__init__()
-        self.type = UsageLimitType.RESPONSE_DRIVEN
-
-    def __str__(self):
-        return f"ResponseDriven[valueType: {self.value_type}, defaultValue: {self.default_value}, value: {self.value}, linkedFeature: {self.linked_features}]"
---
+++ scripts/parsers/FeatureParser.py
@@ -0,0 +1,132 @@
+from typing import Dict, Any
+from scripts.models import ValueType, Feature, FeatureType
+from scripts.models.feature_types import Information, Integration, Domain, Automation, Management, Guarantee, Support, Payment, AutomationType, IntegrationType, PaymentType
+from scripts.parsers.exceptions import PricingParsingException, InvalidDefaultValueException
+
+class FeatureParser:
+    @staticmethod
+    def parse_map_to_feature(feature_name: str, feature_map: Dict[str, Any]) -> 'Feature':
+        feature_type = FeatureType(feature_map["type"])
+
+        if feature_type == FeatureType.INFORMATION:
+            return FeatureParser.parse_map_to_information(feature_name, feature_map)
+        elif feature_type == FeatureType.INTEGRATION:
+            return FeatureParser.parse_map_to_integration(feature_name, feature_map)
+        elif feature_type == FeatureType.DOMAIN:
+            return FeatureParser.parse_map_to_domain(feature_name, feature_map)
+        elif feature_type == FeatureType.AUTOMATION:
+            return FeatureParser.parse_map_to_automation(feature_name, feature_map)
+        elif feature_type == FeatureType.MANAGEMENT:
+            return FeatureParser.parse_map_to_management(feature_name, feature_map)
+        elif feature_type == FeatureType.GUARANTEE:
+            return FeatureParser.parse_map_to_guarantee(feature_name, feature_map)
+        elif feature_type == FeatureType.SUPPORT:
+            return FeatureParser.parse_map_to_support(feature_name, feature_map)
+        elif feature_type == FeatureType.PAYMENT:
+            return FeatureParser.parse_map_to_payment(feature_name, feature_map)
+        else:
+            raise ValueError(f"Unknown feature type: {feature_type}")
+
+    @staticmethod
+    def parse_map_to_information(feature_name: str, feature_map: Dict[str, Any]) -> 'Information':
+        information = Information()
+        information.name = feature_name
+        FeatureParser.set_common_fields(information, feature_name, feature_map)
+        return information
+
+    @staticmethod
+    def parse_map_to_integration(feature_name: str, feature_map: Dict[str, Any]) -> 'Integration':
+        integration = Integration()
+        integration.name = feature_name
+        integration.integration_type = IntegrationType(feature_map["integrationType"])
+        FeatureParser.set_common_fields(integration, feature_name, feature_map)
+        return integration
+
+    @staticmethod
+    def parse_map_to_domain(feature_name: str, feature_map: Dict[str, Any]) -> 'Domain':
+        domain = Domain()
+        domain.name = feature_name
+        FeatureParser.set_common_fields(domain, feature_name, feature_map)
+        return domain
+
+    @staticmethod
+    def parse_map_to_automation(feature_name: str, feature_map: Dict[str, Any]) -> 'Automation':
+        automation = Automation()
+        automation.name = feature_name
+        automation.automation_type = AutomationType(feature_map["automationType"])
+        FeatureParser.set_common_fields(automation, feature_name, feature_map)
+        return automation
+
+    @staticmethod
+    def parse_map_to_management(feature_name: str, feature_map: Dict[str, Any]) -> 'Management':
+        management = Management()
+        management.name = feature_name
+        FeatureParser.set_common_fields(management, feature_name, feature_map)
+        return management
+
+    @staticmethod
+    def parse_map_to_guarantee(feature_name: str, feature_map: Dict[str, Any]) -> 'Guarantee':
+        guarantee = Guarantee()
+        guarantee.name = feature_name
+        FeatureParser.set_common_fields(guarantee, feature_name, feature_map)
+        return guarantee
+
+    @staticmethod
+    def parse_map_to_support(feature_name: str, feature_map: Dict[str, Any]) -> 'Support':
+        support = Support()
+        support.name = feature_name
+        FeatureParser.set_common_fields(support, feature_name, feature_map)
+        return support
+
+    @staticmethod
+    def parse_map_to_payment(feature_name: str, feature_map: Dict[str, Any]) -> 'Payment':
+        payment = Payment()
+        payment.name = feature_name
+        FeatureParser.set_common_fields(payment, feature_name, feature_map)
+        if payment.value_type == ValueType.TEXT:
+            FeatureParser.parse_payment_value(payment, feature_name, feature_map)
+        return payment
+
+    @staticmethod
+    def set_common_fields(feature: 'Feature', feature_name: str, feature_map: Dict[str, Any]):
+        feature.description = feature_map.get("description")
+        feature.value_type = ValueType(feature_map["valueType"])
+
+        try:
+            if feature.value_type == ValueType.NUMERIC:
+                feature.default_value = feature_map["defaultValue"]
+                if not isinstance(feature.default_value, (int, float)):
+                    raise InvalidDefaultValueException(
+                        f"The feature {feature_name} does not have a valid defaultValue. Current valueType: {feature.value_type}; Current defaultValue: {feature_map['defaultValue']}")
+            elif feature.value_type == ValueType.BOOLEAN:
+                feature.default_value = bool(feature_map["defaultValue"])
+            elif feature.value_type == ValueType.TEXT:
+                if isinstance(feature, Payment):
+                    FeatureParser.parse_payment_value(feature, feature_name, feature_map)
+                else:
+                    feature.default_value = str(feature_map["defaultValue"])
+        except KeyError as e:
+            raise InvalidDefaultValueException(f"Missing required field in feature {feature_name}: {str(e)}")
+
+        try:
+            
+            feature.expression = feature_map.get("expression", None)
+            feature.server_expression = feature_map.get("serverExpression", None)
+        except KeyError as e:
+            raise PricingParsingException(f"The feature {feature_name} does not have either an evaluation expression or serverExpression.")
+
+    @staticmethod
+    def parse_payment_value(feature: 'Feature', feature_name: str, feature_map: Dict[str, Any]):
+        allowed_payment_types = feature_map["defaultValue"]
+        if not isinstance(allowed_payment_types, list):
+            f"The feature {feature_name} does not have a not have a valid defaultValue. As it is a PAYMENT feature with valueType = ValueType.TEXT, it should have a list of PaymentOptions as defaultValue. Current defaultValue: {allowed_payment_types}"
+        
+        try:
+            for type in allowed_payment_types:
+                if not hasattr(PaymentType, type):
+                    raise InvalidDefaultValueException(
+                        f"The feature {feature_name} does not have a supported PaymentOptions. PaymentType that generates the issue: {type}")
+        except:
+            raise InvalidDefaultValueException(
+                f"The feature {feature_name} does not have a list of PaymentOptions as default value.")
+        feature.default_value = allowed_payment_types
