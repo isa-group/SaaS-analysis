4e95859fdc6b405f5dc7a6cdf4f3674e2cbf9d38
Alex-GF <yalejandro9@gmail.com>
2024-11-18 17:54:27 +0100

    FEAT: PREPARED EXPERIMENT

--- src/scripts/add-version-fields-to-pricings.ts
+++ src/scripts/add-version-fields-to-pricings.ts
@@ -1,7 +1,9 @@
 import fs from "fs";
 import path from "path";
+import { logExperimentHeader } from "../services/logging.service";
 
-const VERSION_FIELD = "version: '1.0'";
+const VERSION_FIELD = "version:";
+const VERSION_FIELD_VALUE = "version: '1.0'";
 
 // Define the directory path where the YAML files are located
 const directoryPath = path.join(__dirname, "../../data/pricings/yaml/");
@@ -20,7 +22,7 @@ function addVersionFieldToPricings(directoryPath: string) {
       const content = fs.readFileSync(filePath, "utf8");
       if (!content.includes(VERSION_FIELD)) {
         const lines = content.split("\n");
-        lines.splice(1, 0, VERSION_FIELD);
+        lines.splice(1, 0, VERSION_FIELD_VALUE);
         const updatedContent = lines.join("\n");
         fs.writeFileSync(filePath, updatedContent, "utf8");
       }
@@ -28,4 +30,8 @@ function addVersionFieldToPricings(directoryPath: string) {
   }
 }
 
+logExperimentHeader();
+
+console.log(`Adding version field (if missing) to pricings in dataset...`);
 addVersionFieldToPricings(directoryPath);
+console.log('Done!\n');
--- src/scripts/pricings2dzn.ts
+++ src/scripts/pricings2dzn.ts
@@ -6,19 +6,35 @@ import { saveDZNfile } from "pricing4ts";
 const directoryPath = path.join(__dirname, "../../data/pricings/yaml/");
 
 function readYamlFiles(directoryPath: string) {
+  
+  let numberOfParsedFiles = 0;
+  
   const files = fs.readdirSync(directoryPath);
   for (const file of files) {
     const filePath = path.join(directoryPath, file);
-    const pathYear = /(\d{4})/.exec(filePath)?.[0];
+    const pathYear = path.basename(path.dirname(filePath));
     const stat = fs.statSync(filePath);
 
     if (stat.isDirectory()) {
       // Si es un directorio, llama a la función recursivamente
-      readYamlFiles(filePath);
+      numberOfParsedFiles = numberOfParsedFiles + readYamlFiles(filePath);
     } else if (filePath.endsWith(".yml")) {
+      const fileName = path.basename(filePath, ".yml");
+      const outputFilePath = path.join(__dirname, `../../data/pricings/dzn/${pathYear}/${fileName}.dzn`);
+      
+      if (fs.existsSync(outputFilePath)) {
+        numberOfParsedFiles = numberOfParsedFiles + 1;
+        continue;
+      }
+    
       saveDZNfile(filePath, `data/pricings/dzn/${pathYear}`);
+      numberOfParsedFiles = numberOfParsedFiles + 1;
     }
   }
+
+  return numberOfParsedFiles;
 }
 
-readYamlFiles(directoryPath);
+console.log(`Reading YAML files and parsing to MiniZinc datafiles...`);
+const datasetSize = readYamlFiles(directoryPath);
+console.log(`Done! Size of the dataset: ${datasetSize} pricings\n`);
